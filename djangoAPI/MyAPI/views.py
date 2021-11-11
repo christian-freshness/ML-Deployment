02e5f16d02e2360dcccc8a15a265bddf3f527055
christian <christianajulor@gmail.com>
2021-11-10 16:14:59 -0800

    first commit

---
+++ djangoAPI/MyAPI/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render
+from . forms import ApprovalForm
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.core import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib import messages
+from keras import backend as K
+from rest_framework.parsers import JSONParser
+from . models import approvals
+from . serializers import approvalsSerializers
+import pickle
+import joblib
+import json
+import numpy as np
+import sklearn
+from sklearn import preprocessing
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from collections import defaultdict, Counter
+
+# Create your views here.
+
+class ApprovalsView(viewsets.ModelViewSet):
+	queryset = approvals.objects.all()
+	serializer_class = approvalsSerializers
+
+def ohevalue(df):
+	ohe_col=joblib.load("/Users/AJULOR FRESH/Desktop/ML Deployment/djangoAPI/MyAPI/allcol.pkl")
+	cat_columns=['Gender','Married','Education','Self_Employed','Property_Area']
+	df_processed = pd.get_dummies(df, columns=cat_columns)
+	newdict={}
+	for i in ohe_col:
+		if i in df_processed.columns:
+			newdict[i]=df_processed[i].values
+		else:
+			newdict[i]=0
+	newdf=pd.DataFrame(newdict)
+	return newdf
+
+def approvereject(unit):
+	try:
+		mdl=tf.keras.models.load_model("/Users/AJULOR FRESH/Desktop/ML Deployment/djangoAPI/MyAPI/loan_model")
+		scalers=joblib.load("/Users/AJULOR FRESH/Desktop/ML Deployment/djangoAPI/MyAPI/scalers.pkl")
+		X=scalers.transform(unit)
+		y_pred=mdl.predict(X)
+		y_pred=(y_pred>0.50)
+		newdf=pd.DataFrame(y_pred, columns=['Status'])
+		newdf=newdf.replace({True:'Approved', False:'Rejected'})
+		K.clear_session()
+		return (newdf.values[0][0],X[0])
+	except ValueError as e:
+		return (e.args[0])
+
+
+def cxcontact(request):
+	if request.method=='POST':
+		form = ApprovalForm(request.POST)
+		if form.is_valid():
+				Firstname = form.cleaned_data['firstname']
+				Lastname = form.cleaned_data['lastname']
+				Dependents = form.cleaned_data['Dependents']
+				ApplicantIncome = form.cleaned_data['ApplicantIncome']
+				CoapplicantIncome = form.cleaned_data['CoapplicantIncome']
+				LoanAmount = form.cleaned_data['LoanAmount']
+				Loan_Amount_Term = form.cleaned_data['Loan_Amount_Term']
+				Credit_History = form.cleaned_data['Credit_History']
+				Gender = form.cleaned_data['Gender']
+				Married = form.cleaned_data['Married']
+				Education = form.cleaned_data['Education']
+				Self_Employed = form.cleaned_data['Self_Employed']
+				Property_Area = form.cleaned_data['Property_Area']
+				myDict = (request.POST).dict()
+				df=pd.DataFrame(myDict, index=[0])
+				answer=approvereject(ohevalue(df))[0]
+				Xscalers=approvereject(ohevalue(df))[1]
+				if int(df['LoanAmount'])<25000:
+					messages.success(request,'Application Status: {}'.format(answer))
+				else:
+					messages.success(request,'Invalid: Your Loan Request Exceeds $25,000 Limit')
+	
+	form=ApprovalForm()
+				
+	return render(request, 'myform/cxform.html', {'form':form})
+
